d176243aec8738121145264bcca5e635035a92ac
Tiago Bento <1584568+tiagobento@users.noreply.github.com>
2024-04-02 06:26:28 +0000

    2nd

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ ORG="tiagobento"; \
 BRANCH="tb-tests"; \
 PKGS="@kie-tools/boxed-expression-component"; \
 bash <(curl -s https://raw.githubusercontent.com/$ORG/kie-tools/$BRANCH/scripts/sparse-checkout/run.sh) $ORG $BRANCH $PKGS; \
-cd kie-tools; \
+# git clone -b tb-tests --depth 1 https://github.com/tiagobento/incubator-kie-tools
+cd incubator-kie-tools; \
 npm install -g turbo; \
-TURBO_LOG_ORDER=grouped; \
 turbo start -F boxed-expression-component;'
